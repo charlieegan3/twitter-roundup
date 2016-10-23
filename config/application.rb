f7ab4effb8a524fa55bc30c3b681a745b321393d
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-10-23 23:00:26 +0100

    Increase max roundup size allowance

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.roundup_account_limit = 10
+    config.roundup_account_limit = 20
     config.count_display_threshold = 15
   end
 end
