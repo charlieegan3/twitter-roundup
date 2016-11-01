04125f325c46ab72369bbeba9ab5b67e563c88d1
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-11-01 21:18:46 +0000

    Fix username entry placeholder

--- app/helpers/roundup_helper.rb
+++ app/helpers/roundup_helper.rb
@@ -1,13 +1,4 @@
 module RoundupHelper
-  def monitored_accounts_placeholder
-    <<-TEXT.strip_heredoc
-    @rbrampage
-    @newsycombinator
-    @iamdevloper
-    @horse_js
-    TEXT
-  end
-
   def roundup_frequencies
     [['Daily', 0], ['Weekly', 1]]
   end
